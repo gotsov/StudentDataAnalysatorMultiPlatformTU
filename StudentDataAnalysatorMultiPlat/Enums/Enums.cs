63e9470d4ea3e8442e0001adb26d248b2a98aaa1
gotsov <dimitar2200@gmail.com>
2022-04-19 15:05:42 +0300

    Work on main content page and view-model, added excel reader serveces.

---
+++ StudentDataAnalysatorMultiPlat/Enums/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDataAnalysatorMultiPlat.Enums
+{
+    public class Enums
+    {
+        public enum TableTypeEnum
+        {
+            StudentsResultTable,
+            StudentsLogsTable
+        }
+    }
+}
