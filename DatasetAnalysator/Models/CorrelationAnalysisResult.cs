cf776642bac02939c09209df2b37d0d3f0204b1f
Veli-TU <vkalapov@tu-sofia.bg>
2022-05-05 22:24:10 +0300

    Extract log related methods to LogDataHelper

--- DatasetAnalysator/Models/CorrelationAnalysisResult.cs
+++ DatasetAnalysator/Models/CorrelationAnalysisResult.cs
@@ -9,12 +9,10 @@ namespace StudentDataAnalysatorMultiPlat.Models
     public class CorrelationAnalysisResult
     {
         private float corralation;
-        private int mode;
 
         public CorrelationAnalysisResult(float corralation)
         {
             this.corralation = corralation;
-            mode = 1;
 
         }
 
---
+++ DatasetAnalysatorTest/CalculationServices/DispersionOfViewedCoursesServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentDataAnalysatorMultiPlat.Services.CalculationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatasetAnalysatorTest.CalculationServices
+{
+    [TestClass]
+    public class DispersionOfViewedCoursesServiceTest
+    {
+        [TestMethod]
+        public void TestsGetResults()
+        {
+            
+
+
+        }
+    }
+}
